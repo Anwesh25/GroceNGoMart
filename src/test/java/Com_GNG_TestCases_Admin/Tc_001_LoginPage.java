af19a0d78285e23b41a893164295bc28e0e55b6b
Anwesh25 <anweshsriramoju7475@gmail.com>
2025-10-21 20:41:55 +0530

    Anwesh update

--- src/test/java/Com_GNG_TestCases_Admin/Tc_001_LoginPage.java
+++ src/test/java/Com_GNG_TestCases_Admin/Tc_001_LoginPage.java
@@ -3,16 +3,18 @@ package Com_GNG_TestCases_Admin;
 
 import org.testng.annotations.Test;
 
+import Com_BaseClass.Base_Class;
 import Com_GNG_PageObjects_Admin.LoginPage_001;
 
-public class Tc_001_LoginPage extends baseClass{
+
+public class Tc_001_LoginPage extends Base_Class{
 		
 		public LoginPage_001 lPom;
 		
 		@Test
 		public void testing() throws InterruptedException {
 			lPom=new LoginPage_001(driver);
-			lPom.login(rBundle.getString("uname"), rBundle.getString("pwd"));
+			lPom.login(rb.getString("uname"), rb.getString("pwd"));
 			log.info("login sucessfull");
 		}
 	
